051ff742decd76f8c10554efc057e75d7e88242a
Bernardo <beprieto47@gmail.com>
2024-11-17 02:29:24 -0300

    CRUD de livros e configuração do spring security

---
+++ src/test/java/br/com/magementms/ManagementMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.magementms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
